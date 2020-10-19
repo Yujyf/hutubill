9f477a404caed0faa5faa260cbf4420323562382
Yujyf <cf220007@outlook.com>
2020-10-19 16:30:23 +0800

    first commit

---
+++ src/util/DBUtil.java
@@ -0,0 +1,26 @@
+package util;
+  
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+  
+public class DBUtil {
+    static String ip = "127.0.0.1";
+    static int port = 3306;
+    static String database = "hutubill";
+    static String timeZone = "UTC";
+    static String loginName = "root";
+    static String password = "admin";
+    static{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+  
+    public static Connection getConnection() throws SQLException {
+        String url = String.format("jdbc:mysql://%s:%d/%s?useSSL=false&serverTimezone=%s", ip, port, database, timeZone);
+        return DriverManager.getConnection(url, loginName, password);
+    }
+}
